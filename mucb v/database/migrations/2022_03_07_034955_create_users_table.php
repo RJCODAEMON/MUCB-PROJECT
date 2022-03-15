b62f7746de0bc23174de6c1fef7d991721a41c45
vaibhavcodaemonsoftwares <vaibhav.wadkar@codaemonsoftwares>
2022-03-15 17:48:43 +0530

    Vaibhav's MUCB folder.

---
+++ mucb v/database/migrations/2022_03_07_034955_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('userid');
+            $table->string('fname',20);
+            $table->string('lname',20);
+            $table->string('password',32);
+            $table->string('email',32)->unique();
+            $table->string('phone',20);
+            $table->string('mobile',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
