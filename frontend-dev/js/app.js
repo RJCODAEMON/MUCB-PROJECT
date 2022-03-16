40098442e69e0c7e17c39422ca311f366d6547cb
Rohit Jadhav <rohitjadhav8698@gmail.com>
2022-03-16 09:22:51 +0530

    404 & thank-you page created

--- frontend-dev/js/app.js
+++ frontend-dev/js/app.js
@@ -3,3 +3,13 @@ window.addEventListener('scroll', function () {
     let windowPosition = window.scrollY > 0;
     nav.classList.toggle('scrolling-active', windowPosition);
 })
+
+// create password 
+const passBox = document.getElementById('password');
+checkBox = document.getElementById('flexCheckDefault').addEventListener('click', event => {
+    if (event.target.checked) {
+        passBox.classList.add('active')
+    } else {
+        passBox.classList.remove('active')
+    }
+});
